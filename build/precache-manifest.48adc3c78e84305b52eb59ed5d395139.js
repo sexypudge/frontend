c6250c6312e67ce6c432d112a7a962e86ad0b68a
luongnv <luong1c11@gmail.com>
2020-04-07 22:17:20 +0700

    initial

---
+++ build/precache-manifest.48adc3c78e84305b52eb59ed5d395139.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adfee84d168eb5dad7671f1229fb547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e95d6d83b627e4e65e65",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94a2b7b43e99ca67e6f4",
+    "url": "/static/js/2.6da04d33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6da04d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95d6d83b627e4e65e65",
+    "url": "/static/js/main.4bd5a2d4.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
